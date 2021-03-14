dc3dd18389554e32cebe26301fb82b7da8f7d2cb
Michelle Dowling <dowlinmi@gvsu.edu>
2021-03-14 15:02:10 -0400

    Initial commit for all project files

---
+++ ConsoleTypes.java
@@ -0,0 +1,10 @@
+package project3GIVE_TO_STUDENTS;
+
+public enum ConsoleTypes
+{   NoSelection,
+    PlayStation4,
+    XBoxOneS,
+    PlayStation4Pro,
+    NintendoSwitch,
+    SegaGenesisMini; // Best retro
+}
---
+++ ScreenDisplay.java
@@ -0,0 +1,6 @@
+package project3GIVE_TO_STUDENTS;
+
+public enum ScreenDisplay {
+    CurrentRentals,
+    ReturnedItems,
+}
